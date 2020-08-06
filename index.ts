03151851b56b65c80512c757e5117a329255defd
Gintautas Kazlauskas <gintautaskazlauskas@protonmail.com>
2020-08-06 16:43:56 +0300

    JWT done

--- index.ts
+++ index.ts
@@ -1,15 +1,41 @@
-import * as express from "express";
+import express from "express";
+import categories from "./apps.json";
+import * as jwt from "jsonwebtoken";
+const jwtSecret = "linite420";
+let jwtHeader = "";
+let jwtPayload = "";
+let jwtSignature = "";
 
 const app = express();
 const port = 8080;
 
 app.set("view engine", "pug");
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.render("index");
+  res.render("index", categories);
+});
+
+app.post("/apps", (req, res) => {
+  const tokenArr = jwt.sign(req.body, jwtSecret).split(".");
+  jwtHeader = tokenArr[0];
+  jwtPayload = tokenArr[1];
+  jwtSignature = tokenArr[2];
+
+  console.log(jwtPayload);
+  res.send(jwtPayload);
+});
+
+app.get("/*", (req, res) => {
+  const payload = req.url.replace("/", "");
+  const decoded = jwt.verify(
+    [jwtHeader, payload, jwtSignature].join("."),
+    jwtSecret
+  )["apps"];
+  res.send(`Dude nice! You're getting ${JSON.stringify(decoded)}`);
 });
 
 app.listen(port, () => {
-    console.log(`Serving app on http://localhost:${port}/`);
+  console.log(`Serving app on http://localhost:${port}/`);
 });
--- public/main.js
+++ public/main.js
@@ -1,18 +1,50 @@
-const checks = document.querySelectorAll('ul.apps li label')
-const title = document.querySelector('div.container h1.title')
-const subtitle = document.querySelector('div.container h2.subtitle')
-const apps = []
+const checks = document.querySelectorAll("ul.apps li label");
+const title = document.querySelector("div.container h1.title");
+const subtitle = document.querySelector("div.container h2.subtitle");
+const terminal = document.querySelector("div.terminal");
+const command = document.querySelector("div.terminal p.command");
+const apps = [];
 
-console.log(window.navigator.oscpu)
+let appCode = "";
 
-checks.forEach((currentValue) => { // Checkbox checking
-    currentValue.addEventListener('change', () => {
-        if (currentValue.children[0].checked) {
-            apps.push(currentValue.children[2].innerHTML)
-            title.innerHTML = apps
-        } else {
-            apps.pop(currentValue.children[2].innerHTML)
-            title.innerHTML = apps
-        }
-    })
-})
+async function getAppCode() {
+  code = await fetch("/apps", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({apps: apps}),
+  })
+    .then((response) => response.text())
+    .then((response) => {
+      appCode = response;
+      command.innerHTML = "curl http://localhost:8080/" + response;
+      testREST(response);
+    });
+}
+
+async function testREST(code) {
+  resp = await fetch("/" + code, {
+    method: "GET",
+    headers: {"Content-Type": "application/json"},
+  })
+    .then((response) => response.text())
+    .then((response) => {
+      console.log(response);
+    });
+}
+
+checks.forEach((currentValue) => {
+  currentValue.addEventListener("change", () => {
+    if (apps) {
+      terminal.hidden = false;
+    } else {
+      terminal.hidden = true;
+    }
+    if (currentValue.children[0].checked) {
+      apps.push(currentValue.children[2].innerHTML);
+      getAppCode();
+    } else {
+      apps.pop(currentValue.children[2].innerHTML);
+      getAppCode();
+    }
+  });
+});
